b5de0d29e4a33cbfb295afafa7501a2c0642e844
Michael Cox <michael.cox@blueyonder.co.uk>
2020-11-15 23:36:55 +0000

    Latest Update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,13 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            return 'F';
+        }
     }
 }
