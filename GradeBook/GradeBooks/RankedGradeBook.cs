988ad1bc792f66a7175d302384376e01affee437
Michael Cox <michael.cox@blueyonder.co.uk>
2020-11-16 00:11:38 +0000

    update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,7 +35,10 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
+            {
                 Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
 
             base.CalculateStatistics();
         }
@@ -43,7 +46,10 @@ namespace GradeBook.GradeBooks
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
+            {
                 Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
 
             base.CalculateStudentStatistics(name);  
         }
